7f1fdc928b5dad2cb98b399cc1721d1a118cdfb6
Martin Knapp <martin@bookable.online>
2025-11-05 07:28:26 +0000

    navbar update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -40,6 +40,7 @@ export default function Navbar({ transparent = false }: { transparent?: boolean 
           {/*<NavLink to="/readings" className={({isActive}) => `${link} ${isActive?active:''}`}>Book A Session</NavLink> */}
           <NavLink to="/blog" className={({isActive}) => `${link} ${isActive?active:''}`} >Articles</NavLink>
           <NavLink to="/kabbalah" className={({isActive}) => `${link} ${isActive?active:''}`} >Kabbalah</NavLink>
+          <NavLink to="/spiritual-resources" className={({isActive}) => `${link} ${isActive?active:''}`} >Resources</NavLink>
           <NavLink to="/contact" className={({isActive}) => `${link} ${isActive?active:''}`}>Contact</NavLink>
           
         
